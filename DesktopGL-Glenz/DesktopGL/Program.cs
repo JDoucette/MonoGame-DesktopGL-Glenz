8c46e5ef2b294e7fa11e09f8e6b14b3b8c97c4a6
Jason Doucette <jdoucette@gmail.com>
2023-09-06 20:00:52 -0700

    empty solution with both DesktopGL and WindowsDX projects

---
+++ DesktopGL-Glenz/DesktopGL/Program.cs
@@ -0,0 +1,9 @@
+﻿
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		using var game = new DesktopGL.Game_DesktopGL();
+		game.Run();
+	}
+}
---
+++ DesktopGL-Glenz/WindowsDX/Program.cs
@@ -0,0 +1,9 @@
+﻿
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		using var game = new WindowsDX.Game_WindowsDX();
+		game.Run();
+	}
+}
